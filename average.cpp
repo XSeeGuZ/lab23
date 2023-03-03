37bba488b0a55a7e1250d8d124acb016417ef59c
TelTRiNoZ <title34718@gmail.com>
2023-03-03 15:24:29 +0700

    complete

--- average.cpp
+++ average.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
+int main(int argc,char* argv[]){
+if (argc==0 || argc==1) cout << "Please input numbers to find average.\n"; //argc จะเท่ากับ 1 เสมอ ไม่จำเป็นต้องใส่ argc==0 ก็ได้
+else{
+    double sum=0;
+    double avg=0;
+    for (int i = 1; i < argc; i++)
+    {
+        sum += atof(argv[i]);
+    }
+    avg=sum/(argc-1);
+    cout << "---------------------------------\n";
+    cout << "Average of " << argc-1 << " numbers = " << avg << endl;
+    cout << "---------------------------------";
+    }
+}
